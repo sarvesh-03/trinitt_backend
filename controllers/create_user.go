a32f859a9d1e08dc40fcdf5fd71b71c59feefd64
sarvesh-03 <sarveshjan18@gmail.com>
2023-02-11 07:52:38 +0530

    avro

--- controllers/create_user.go
+++ controllers/create_user.go
@@ -1,10 +1,12 @@
 package controllers
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/hamba/avro"
 	"github.com/labstack/echo/v4"
 	"github.com/segmentio/kafka-go"
 	"github.com/trinitt/config"
@@ -19,24 +21,57 @@ type SignupRequest struct {
 	IsSeller  bool   `json:"is_seller"`
 }
 
+var Schema = `{
+	"type": "record",
+	"name": "simple",
+	"namespace": "org.hamba.avro",
+	"fields" : [
+		{"name": "a", "type": "long"},
+		{"name": "b", "type": "string"}
+	]
+}`
+
+type SimpleRecord struct {
+	A int64  `avro:"a"`
+	B string `avro:"b"`
+}
+
+
 func SignupUser(c echo.Context) error {
 
-
+	schema, err := avro.Parse(Schema)
+	if err != nil {
+		log.Fatal("failed to write messages:", err)
+	}
 	producer:= config.GetProducer()
 	
+	in := SimpleRecord{A: 27, B: "foo"}
+
+	data, err := avro.Marshal(schema, in)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", data)
+
+
+
+	// Writing OCF data
+	
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// // if err := producer.Close(); err != nil {
+	// // 	log.Fatal("failed to close writer:", err)
+	// // }
+	// fmt.Println(ocfFileContents.String())
 	producer.SetWriteDeadline(time.Now().Add(10*time.Second))
-	_, err := producer.WriteMessages(
-		kafka.Message{Value: []byte("one!")},
-		kafka.Message{Value: []byte("two!")},
-		kafka.Message{Value: []byte("three!")},
+	_, err = producer.WriteMessages(
+		kafka.Message{Value: data},
 	)
 	if err != nil {
 		log.Fatal("failed to write messages:", err)
 	}
-
-	// if err := producer.Close(); err != nil {
-	// 	log.Fatal("failed to close writer:", err)
-	// }
 
 	return utils.SendResponse(c, http.StatusOK, "User created successfully")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/fatih/color v1.13.0
 	github.com/golang-jwt/jwt/v4 v4.4.3
+	github.com/hamba/avro v1.8.0
 	github.com/joho/godotenv v1.4.0
 	github.com/labstack/echo/v4 v4.9.0
 	github.com/segmentio/kafka-go v0.4.38
@@ -27,11 +28,14 @@ require (
 	github.com/jackc/pgx/v4 v4.17.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/labstack/echo-jwt/v4 v4.1.0
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
